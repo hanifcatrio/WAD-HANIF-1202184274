095b693b57f69696f33e29b0b84ecb479ae453b6
unknown <hanifcatrio@gmail.com>
2020-11-22 23:24:38 +0630

    TP Modul 4

---
+++ MODUL4_HANIF/index.php
@@ -0,0 +1,90 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <title>Index</title>
+</head>
+
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">WAD Beauty</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+    <a class="nav-link" href="cart.php"><img src="cart.png" width = "30" height = "30"></a>
+
+      <li class="nav-item active">
+        <a class="nav-link" href="login.php">Selamat datang </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="profile.php">Profile</a>
+      </li>
+</nav>
+<body>
+<div class="container">
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h2 align="center" class="h2">WAD BEAUTY</h2>
+    <p align="center" class="lead">Tersedia Skin Care dengan harga murah tapi bukan murahan dan berkualitas</p>
+  </div>
+</div>
+</div>
+<div class="container">
+<div class="card-group">
+  <div class="card">
+    <img class="card-img-top" src="yuja.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">YUJA NIACIN 30 DAYS BLEMISH CARE SERUM</h5>
+      <p class="card-text">Produk terbaru dari somebymi yang memiliki manfaat untuk Whitening + blemish care + anti wrinkle, sangat recomended untuk masalah kulit kusam kering dan bekas jerawat atau FLEK hitam </p>
+      <p class="card-text">RP.169.000</p>
+      <input class="btn btn-primary" type="Tambahkan ke Keranjang" value="Tambahkan ke Keranjang">
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="somebymi.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">SOMEBY Snail Truecica Miracle Repair Cream</h5>
+      <p class="card-text">Sebagai pelembap, krim ini mampu memberikan kelembapan yang menyeluruh dan tahan lama bagi kulit sehingga terasa halus. lembap dan kenyal mencerahkan menghambat penuaan seperti keriput dan garis halus juga juga menenangkan kulit yang iritasi, dengan kandungan 420,000ppm Snail Truecica</p>
+      <p class="card-text">RP.180.000</p>
+      <input class="btn btn-primary" type="Tambahkan ke Keranjang" value="Tambahkan ke Keranjang">
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="30days.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">30 DAYS MIRACLE TONER</h5>
+      <p class="card-text">Dengan kandungan AHA, BHA dan PHA bekerja secara efektif untuk membuat kulit lebih bersih dan lebih bersinar, mengandung 10.000 ppm ekstrak pohon teh alami yang secara efektif meningkatkan elastisitas dan menutrisi kulit Anda tanpa efek iritasi karena tidak mengandung 20 bahan 500 dan pewarna berbahaya</p>
+      <p class="card-text">RP.108.000</p>
+      <input class="btn btn-primary" type="Tambahkan ke Keranjang" value="Tambahkan ke Keranjang">
+    </div>
+  </div>
+</div>
+</div>
+
+
+</div>
+    
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
